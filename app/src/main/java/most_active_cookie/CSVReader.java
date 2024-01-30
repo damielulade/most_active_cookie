54bbc3740eec7d92d2fb512fedaba81bc4b8bd30
Dami Elulade <dami_elulade@outlook.com>
2024-01-30 00:55:45 +0000

    refactor: Change handleInputStream to use streams

--- app/src/main/java/most_active_cookie/CSVReader.java
+++ app/src/main/java/most_active_cookie/CSVReader.java
@@ -3,32 +3,29 @@ package most_active_cookie;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
+
+import javax.swing.text.html.Option;
 
 public class CSVReader {
 
-  public CSVReader() { }
+  public CSVReader() {
+  }
 
-  private Optional<String> parseInputStream(InputStream in) {
+  private Stream<String> handleInputStream(InputStream in) {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+    return bufferedReader.lines();
+  }
+
+  public Optional<String> getFileContents(InputStream in) {
     try {
-      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-      StringBuilder stringBuilder = new StringBuilder();
-
-      String line;
-
-      while ((line = bufferedReader.readLine()) != null) {
-        stringBuilder.append(line);
-      }
-      return Optional.of(stringBuilder.toString());
-
+      String result = handleInputStream(in).reduce("", (temp, line) -> temp + line);
+      return Optional.of(result);
     } catch (Exception e) {
       System.err.println(e);
       return Optional.empty();
     }
   }
-
-  public Optional<String> getFileContents(InputStream in) {
-    return parseInputStream(in);
-  }
-
 }
